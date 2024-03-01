f7f90fdba23c8eca2c4a8b00cd23dee12c02d663
nelsonamaran14 <amarannelz14@gmail.com>
2024-03-01 16:32:00 +0530

    sbi_changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,11 @@ import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { SidemenucardComponent } from './sidemenucard/sidemenucard.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table'; 
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
-import { FormComponent } from './page/form/form.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -20,7 +17,6 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MyPrototypeComponent } from './my-prototype/my-prototype.component';
 import { PrototypeComponent } from './prototype/prototype.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -28,9 +24,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 @NgModule({
   declarations: [
     AppComponent,
-    SidemenucardComponent,
-    FormComponent,
-    MyPrototypeComponent,
     PrototypeComponent,
    
   ],
